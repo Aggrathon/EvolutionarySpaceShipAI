e1b00a496a8464e745aa8393ba0e122d95c98666
Aggrathon <antonbjo@gmail.com>
2017-03-23 19:13:00 +0200

    Chromosome class

--- evolutionary_track.py
+++ evolutionary_track.py
@@ -5,188 +5,187 @@ import math
 from timeit import default_timer as timer
 
 
+def random_curve_limit(min_val, max_val):
+    delta = max_val - min_val
+    r1 = random.random()*delta+min_val
+    r2 = np.random.triangular(min_val, min_val+delta/2, max_val)
+    return (r1+r2)/2
+
+
 class Chromosome(object):
 
-	def __init__(self, size: int=10, gene=None):
-		self.size = size
-		if gene is not None:
-			pass
+	def __init__(self, size: int=10, genes=None, other=None):
+		if other is None:
+			self.size = size
+			self.__fitness__ = -1
+			if genes is None:
+				self.genes = list(range(size))
+				np.random.shuffle(self.genes)
+			else:
+				self.genes = genes
+		else:
+			self.size = other.size
+			self.genes = list(other.genes)
+			self.__fitness__ = other.__fitness__
 
-def generate_pop(size: int = 10):
-	arr = [i for i in range(size)]
-	np.random.shuffle(arr)
-	return arr
+	def copy(self):
+		return Chromosome(other=self)
 
-def mutate_pop(population, mutate_chance: float = 0.02, reverse_chance=0.5):
-	pop = list(population)
-	length = len(population)
-	for i in range(length):
-		if random.random() < mutate_chance:
-			o = random.randint(0,length-2)
-			if o == i:
-				o = length-1
-			if random.random() < reverse_chance:
-				if len(pop) != length:
-					print("error", i, o, ' ',len(pop), length)
-				if o < i:
-					pop = pop[:o+1]+pop[i-1:o:-1]+pop[i:]
+	def get_track(self, points):
+		return [points[i] for i in self.genes]
+
+	def get_fitness(self, points, scale):
+		if self.__fitness__ != -1:
+			return self.__fitness__
+		return track.track_length(self.get_track(points), scale)
+
+	def mutate(self, mutate_chance: float = 0.02, reverse_chance=0.5):
+		mutated = self.copy()
+		for i in range(self.size):
+			if random.random() < mutate_chance:
+				o = random.randint(0, self.size-2)
+				if o == i:
+					o = self.size-1
+				if random.random() < reverse_chance:
+					if o < i:
+						mutated.genes = mutated.genes[:o+1]+mutated.genes[i-1:o:-1]+mutated.genes[i:]
+					else:
+						mutated.genes = mutated.genes[:i+1]+mutated.genes[o-1:i:-1]+mutated.genes[o:]
 				else:
-					pop = pop[:i+1]+pop[o-1:i:-1]+pop[o:]
-				if len(pop) != length:
-					print("error", i, o, ' ',len(pop), length)
-			else:
-				tmp = pop[o]
-				pop[o] = pop[i]
-				pop[i] = tmp
-	return pop
+					tmp = mutated.genes[o]
+					mutated.genes[o] = mutated.genes[i]
+					mutated.genes[i] = tmp
+		return mutated
 
-def crossover_simple(pop1, pop2):
-	splitf = np.random.normal(0.4, 0.1)
-	if splitf < 0.1: splitf = 0.1
-	elif splitf > 0.7: splitf = 0.7
-	split = int(splitf*len(pop1))
-	npop1 = pop1[:split]
-	for i in range(split,len(pop2)):
-		x = pop2[i]
-		if not x in npop1:
-			npop1.append(x)
-	for i in range(split):
-		x = pop2[i]
-		if not x in npop1:
-			npop1.append(x)
-		if len(npop1) == len(pop1):
-			break
-	npop2 = pop2[:split]
-	for i in range(split,len(pop1)):
-		x = pop1[i]
-		if not x in npop2:
-			npop2.append(x)
-	for i in range(split):
-		x = pop1[i]
-		if not x in npop2:
-			npop2.append(x)
-		if len(npop2) == len(pop1):
-			break
-	return npop1, npop2
+	def crossover_simple(self, other):
+		split = int(random_curve_limit(0.2, 0.8) * self.size)
+		def combine(pop1, pop2):
+			npop = pop1[:split]
+			for i in range(split, len(pop2)):
+				x = pop2[i]
+				if not x in npop:
+					npop.append(x)
+			for i in range(split):
+				x = pop2[i]
+				if not x in npop:
+					npop.append(x)
+				if len(npop) == len(pop1):
+					break
+			return Chromosome(self.size, npop)
+		return combine(self.genes, other.genes), combine(other.genes, self.genes)
 
-def crossover_dmx(pop1, pop2):
-	def gen(split, pop1, pop2):
-		npop = pop1[:split]
-		for i in pop2[split:]:
-			if i not in npop:
-				npop.append(i)
-		for i in pop2[:split]:
-			if i not in npop:
-				npop.append(i)
-		return npop
-	splitf = np.random.normal(0.4, 0.1)
-	if splitf < 0.1: splitf = 0.1
-	elif splitf > 0.7: splitf = 0.7
-	split = int(splitf*len(pop1))
-	return gen(split, pop1, pop2), gen(split, pop2, pop1)
+	def crossover_dmx(self, other):
+		def gen(split, pop1, pop2):
+			npop = pop1[:split]
+			for i in pop2[split:]:
+				if i not in npop:
+					npop.append(i)
+			for i in pop2[:split]:
+				if i not in npop:
+					npop.append(i)
+			return Chromosome(self.size, npop)
+		split = int(random_curve_limit(0.2, 0.8) * self.size)
+		return gen(split, self.genes, other.genes), gen(split, other.genes, self.genes)
 
-def crossover_erx(pop1, pop2):
-	for x in pop1:
-		if x not in pop2:
-			print("sad")
-	def gen(pop1, pop2):
-		npop = [random.choice(pop1)]
-		for i in range(len(pop1)-1):
-			poss = []
-			i1 = pop1.index(npop[i])
-			i2 = pop2.index(npop[i])
-			if pop1[i1-1] not in npop:
-				poss.append(pop1[i1-1])
-			if pop1[(i1+1)%len(pop1)] not in npop:
-				poss.append(pop1[(i1+1)%len(pop1)])
-			if pop2[i2-1] not in npop:
-				poss.append(pop2[i2-1])
-			if pop2[(i2+1)%len(pop2)] not in npop:
-				poss.append(pop2[(i2+1)%len(pop2)])
-			if len(poss) == 0:
-				for i in pop1:
-					if i not in npop:
-						npop.append(i)
-						break
-			else:
-				npop.append(random.choice(poss))
-		return npop
-	return gen(pop1, pop2), gen(pop2, pop1)
+	def crossover_erx(self, other):
+		def gen(pop1, pop2):
+			npop = [random.choice(pop1)]
+			for i in range(len(pop1)-1):
+				poss = []
+				i1 = pop1.index(npop[i])
+				i2 = pop2.index(npop[i])
+				if pop1[i1-1] not in npop:
+					poss.append(pop1[i1-1])
+				if pop1[(i1+1)%len(pop1)] not in npop:
+					poss.append(pop1[(i1+1)%len(pop1)])
+				if pop2[i2-1] not in npop:
+					poss.append(pop2[i2-1])
+				if pop2[(i2+1)%len(pop2)] not in npop:
+					poss.append(pop2[(i2+1)%len(pop2)])
+				if len(poss) == 0:
+					for i in pop1:
+						if i not in npop:
+							npop.append(i)
+							break
+				else:
+					npop.append(random.choice(poss))
+			return Chromosome(self.size, npop)
+		return gen(self.genes, other.genes), gen(other.genes, self.genes)
 
 
 def generate_pops(num: int = 10, size : int = 20):
-	return [generate_pop(size) for _ in range(10)]
+	return [Chromosome(size) for _ in range(10)]
 
 def mutate_pops(pops, percentage : float = 0.02):
-	return [mutate_pop(pop, percentage) for pop in pops]
+	return [pop.mutate(percentage) for pop in pops]
 
-def crossover_pops(pops, method, times = 1):
-	length = len(pops)-1
+def crossover_pops(pops):
 	result = []
-	for i in range(times):
-		np.random.shuffle(pops)
-		for i in range(0,length,2):
-			p1, p2 = method(pops[i], pops[i+1])
-			result.append(p1)
-			result.append(p2)
+	np.random.shuffle(pops)
+	for i in range(0, len(pops)-1, 2):
+		p1, p2 = pops[i].crossover_simple(pops[i+1])
+		result.append(p1)
+		result.append(p2)
+	np.random.shuffle(pops)
+	for i in range(0, len(pops)-1, 2):
+		p1, p2 = pops[i].crossover_erx(pops[i+1])
+		result.append(p1)
+		result.append(p2)
+	np.random.shuffle(pops)
+	for i in range(0, len(pops)-1, 2):
+		p1, p2 = pops[i].crossover_dmx(pops[i+1])
+		result.append(p1)
+		result.append(p2)
 	return result
-
-def get_track(pop, points):
-	return [points[i] for i in pop]
-
-def get_fitness(pop, points, scale):
-	return track.track_length(get_track(pop, points), scale)
 
 def select_pops(pops, points, amount: int = 10, scale : float = 1.0, roulette: bool=False):
 	if roulette:
-		norm_len = [get_fitness(p, points, scale) for p in pops]
+		norm_len = [p.get_fitness(points, scale) for p in pops]
 		norm_max = np.max(norm_len)+1
 		inv_len = [1-l/norm_max for l in norm_len]
 		inv_sum = np.sum(inv_len)
 		fitness = np.multiply(inv_len, 1/inv_sum)
 		selection = np.random.choice(np.arange(len(pops)), size=amount, p=fitness)
-		return [pops[i] for i in sorted(selection, reverse=True, key=lambda s: fitness[s])]
+		selection.sort(reverse=True, key=lambda s: fitness[s])
+		return [pops[i] for i in selection]
 	else:
-		pops.sort(key=lambda p: get_fitness(p, points, scale))
+		pops.sort(key=lambda p: p.get_fitness(points, scale))
 		return pops[:amount]
 
-def evolutionary_generate(points, scale, generation_size = 40, max_generations=200,
-		min_improvment = 0.005, stalled_generations = 15, elitism=True):
+def evolutionary_generate(points, scale, generation_size = 40, max_generations=250,
+		min_improvment = 0.005, stalled_generations = 20, elitism=True):
 	track_length = len(points)
 	population = generate_pops(generation_size, track_length)
-	best = track.track_length(get_track(population[0], points), scale)
+	best = population[0].get_fitness(points, scale)
 	counter = 0
 	for i in range(max_generations):
 		pool = generate_pops(generation_size//4, track_length)
-		parents = population + pool
-		pool += crossover_pops(parents, crossover_simple)
-		pool += crossover_pops(parents, crossover_dmx)
-		pool += crossover_pops(parents, crossover_erx)
-		pool += mutate_pops(pool, (1-i/max_generations)*0.04+0.01)
+		pool += crossover_pops(population + pool)
+		pool += mutate_pops(pool, (1-i/max_generations)*0.05+0.01)
 		if elitism:
 			sel_pop = select_pops(pool, points, generation_size-(generation_size//8), scale)
 			elit_pop = population[:generation_size//8]
-			if get_fitness(sel_pop[0], points, scale) > get_fitness(elit_pop[0], points, scale):
+			if sel_pop[0].get_fitness(points, scale) > elit_pop[0].get_fitness(points, scale):
 				population = elit_pop+sel_pop
 			else:
 				population = sel_pop+elit_pop
 		else:
 			population = select_pops(pool, points, generation_size, scale)
-		nb = get_fitness(population[0], points, scale)
+		nb = population[0].get_fitness(points, scale)
 		if best - min_improvment > nb:
 			best = nb
 			counter = 0
-		if i%5 == 0:
-			print("Generation %d (%.2f)"%(i, nb))
+		#if i%10 == 0:
+		#	print("Generation %d (%.2f)"%(i, nb))
 		counter += 1
 		if counter > stalled_generations:
 			print("Final Generation %d (%.2f)"%(i, nb))
 			break
-	return get_track(population[0], points)
+	return population[0].get_track(points)
 
 def __compare_method__(track_length=30):
 	time1 = timer()
-	t = track.Track(1, track_length, None)
+	t = track.Track(1, track_length)
 	time2 = timer()
 	solution = evolutionary_generate(t.points, t.scale)
 	time3 = timer()
